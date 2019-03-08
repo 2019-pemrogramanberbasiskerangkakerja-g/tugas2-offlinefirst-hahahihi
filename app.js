c869023d3370e3075e376054cc2d1812bd6318d8
Naufal Pranasetyo <naufalpf@gmail.com>
2019-03-08 15:50:08 +0700

    app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// import dependentcies
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -8,21 +9,35 @@ const flash = require('connect-flash');
 const config = require('./config/database');
 const passport = require('passport');
 const mcache = require('memory-cache');
+
+
+// init app
 const app = express();
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
 app.use(bodyParser.json());
+
+//set public static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//express session middleware
 app.use(session({
   secret: 'keyboard cat',
   resave: true,
   saveUninitialized: true,
 }));
+
+//express messages middleware
 app.use(require('connect-flash')());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
 });
+
+//express validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -39,17 +54,27 @@ app.use(expressValidator({
     };
   }
 }));
+
 require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
+
+// global variable user
 app.get('*', function(req, res, next){
 	res.locals.user = req.user || null;
 	next();
 });
+
+//load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','pug');
+
+//Route Group
 let users = require('./routes/users');
 app.use('/', users);
+
+
+// start server
 app.listen(3000, function(){
 	console.log('Server Started on port 3000..');
 });
